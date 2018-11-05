2c17cf389164fa41ec1607ccebdca08edd715379
aonemd <aonemdsaleh@gmail.com>
2018-11-05 21:28:38 +0200

    Add the ability to post new comments

--- src/assets/js/comments.js
+++ src/assets/js/comments.js
@@ -1,4 +1,9 @@
 import commentStyles from '../css/comments.css'
+const moment = require('moment');
+const script = document.currentScript;
+
+let issueNumber  = script.getAttribute('issue');
+
 
 document.getElementById('login').onclick = function() {
   window.location.href = `https://github.com/login/oauth/authorize?client_id=fe4931bc81e99ec2522f&redirect_uri=${window.location.href}&scope=public_repo`
@@ -30,10 +35,23 @@ if (localStorage.getItem('GH_ACCESS_TOKEN')) {
   document.getElementById('unlogged-in-message').style.display = 'none';
 }
 
-const moment = require('moment');
-const script = document.currentScript;
+document.getElementById('comments__add-button').onclick = function() {
+  let github_access_token = localStorage.getItem('GH_ACCESS_TOKEN');
+  let comment_content     = document.getElementById('comments__editor').value;
 
-let issueNumber  = script.getAttribute('issue');
+  window.fetch(`https://jazl-server.herokuapp.com/issues/${issueNumber}/comments`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${github_access_token}`
+    },
+    body: JSON.stringify({ content: comment_content })
+  }).then(res => {
+    document.getElementById('comments__editor').value = '';
+  });
+}
 
 window.fetch(`https://jazl-server.herokuapp.com/issues/${issueNumber}/comments`,
   { Accept: 'application/json' }).then((response) => {
